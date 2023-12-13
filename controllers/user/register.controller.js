8c17864ce56d6ea407b280cd425803169e091db2
Leo0041 <jespitiame@uninpahu.edu.co>
2023-12-12 23:28:57 -0500

    12/12/2023

--- controllers/user/register.controller.js
+++ controllers/user/register.controller.js
@@ -1,5 +1,15 @@
+import validateSchema from '../../helpers/validate.helpers.js';
+import schema from '../../schemas/user/register.schema.js';
+
 const main = async (req, res, next) => {
+    try {
     //recibir info en req que vamos a tener que validar
+    await validateSchema(schema, req, res, next)
     const { email, username, password } = req.body;
+        
+    } catch (error) {
+        next(error);
+    }
+
 
 };
